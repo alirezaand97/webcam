8a84ada42b783cdeca730f1e2e343ec05381c67a
Alireza Alinia <a.alinia@asax.ir>
2023-01-18 18:36:14 +0330

    third commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 
 import React from "react";
 import WebcamImage from "./WebcamImage";
+import WebcamVideo from "./WebcamVideo";
 
 function App() {
 
   return (
     <div className="App">
       {/* <WebcamImage/> */}
-      <WebcamVideo/>
+      <WebcamVideo />
     </div>
   );
 }
